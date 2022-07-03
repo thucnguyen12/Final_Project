5a2ce7ba596fc1c95da45404e9caddd52d0feffb
thucnguyen12 <duythuc12@gmail.com>
2022-07-03 15:01:43 +0700

    BOOTLOADER FISRT COMMIT

---
+++ Core/Src/main.c
@@ -0,0 +1,303 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "flash.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+#define APP_ADDR 0x08040000
+#define OTA_ADDR 0x08020000
+#define UPDATE_CHECK_ADDR 0x0801FC00
+#define CHECK_UPDATE_VALUE 0xAAAAAAAA
+#define SIZE_OF_FIRM_ADDR 0x0801FCFF
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ CRC_HandleTypeDef hcrc;
+
+/* USER CODE BEGIN PV */
+uint32_t check_update_value;
+uint32_t code_data_temp;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_CRC_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void update_firmware (void)
+{
+	uint32_t size_of_firmware = Flash_Read_Uint (SIZE_OF_FIRM_ADDR);
+	uint32_t word_to_write = size_of_firmware / 4;
+	uint32_t word_wrote = 0;
+	uint32_t word_temp[1024];
+	while (word_wrote < word_to_write)
+	{
+		Flash_Read_Array_32bit (word_temp, OTA_ADDR + word_wrote * sizeof (word_temp), sizeof (word_temp));
+		if (Flash_Write_Array_32bit (word_temp, APP_ADDR + word_wrote * sizeof (word_temp), sizeof (word_temp)) != 1)
+		{
+			Error_Handler();
+		}
+		word_wrote += 1024;
+	}
+	if (size_of_firmware % 4)
+	{
+//		Flash_Read_Array_32bit (word_temp, OTA_ADDR + word_wrote * sizeof (word_temp));
+		if ((size_of_firmware % 4) == 1)
+		{
+			uint32_t the_rest_word = Flash_Read_Uint (OTA_ADDR + word_wrote * sizeof (word_temp));
+			the_rest_word |= 0x00FFFFFF;
+			if (Flash_Write_Uin32t (the_rest_word, APP_ADDR + word_wrote * sizeof (word_temp)))
+			{
+				Error_Handler();
+			}
+		}
+		else if ((size_of_firmware % 4) == 2)
+		{
+			uint32_t the_rest_word = Flash_Read_Uint (OTA_ADDR + word_wrote * sizeof (word_temp));
+			the_rest_word |= 0x0000FFFF;
+			if (Flash_Write_Uin32t (the_rest_word, APP_ADDR + word_wrote * sizeof (word_temp)))
+			{
+				Error_Handler();
+			}
+		}
+		else if ((size_of_firmware % 4) == 3)
+		{
+			uint32_t the_rest_word = Flash_Read_Uint (OTA_ADDR + word_wrote * sizeof (word_temp));
+			the_rest_word |= 0x000000FF;
+			if (Flash_Write_Uin32t (the_rest_word, APP_ADDR + word_wrote * sizeof (word_temp)))
+			{
+				Error_Handler();
+			}
+			the_rest_word = the_rest_word << 16;
+		}
+	}
+//	Flash_Erase (UPDATE_CHECK_ADDR, 1);
+	Flash_Write_Uin32t (0xFFFFFFFF, UPDATE_CHECK_ADDR);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_CRC_Init();
+  /* USER CODE BEGIN 2 */
+//  check_update_value = Flash_Read_Uint (UPDATE_CHECK_ADDR);
+  if (check_update_value == CHECK_UPDATE_VALUE)
+  {
+//	  update_firmware ()
+  }
+  else
+  {
+	  //jumpto main
+  }
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 240;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief CRC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CRC_Init(void)
+{
+
+  /* USER CODE BEGIN CRC_Init 0 */
+
+  /* USER CODE END CRC_Init 0 */
+
+  /* USER CODE BEGIN CRC_Init 1 */
+
+  /* USER CODE END CRC_Init 1 */
+  hcrc.Instance = CRC;
+  if (HAL_CRC_Init(&hcrc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CRC_Init 2 */
+
+  /* USER CODE END CRC_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM2 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM2) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ flash_fucntion/flash.c
@@ -0,0 +1,250 @@
+
+#include "flash.h"
+
+//__static_funtion___
+
+//static void deleteBuffer8(uint8_t* data, uint16_t _LENGTH_);
+//static void deleteBuffer16(uint16_t* data, uint16_t _LENGTH_);
+static void deleteBuffer32(uint32_t* data, uint16_t _LENGTH_);
+
+//______________________________________________________________________________________________________
+
+
+/* Base address of the Flash sectors */
+#define ADDR_FLASH_SECTOR_0    ((uint32_t)0x08000000) /* Base @ of Sector 0, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_1    ((uint32_t)0x08004000) /* Base @ of Sector 1, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_2    ((uint32_t)0x08008000) /* Base @ of Sector 2, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_3    ((uint32_t)0x0800C000) /* Base @ of Sector 3, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_4    ((uint32_t)0x08010000) /* Base @ of Sector 4, 64 Kbytes */
+#define ADDR_FLASH_SECTOR_5    ((uint32_t)0x08020000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_6    ((uint32_t)0x08040000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_7    ((uint32_t)0x08060000) /* Base @ of Sector 7, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_8    ((uint32_t)0x08080000) /* Base @ of Sector 8, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_9    ((uint32_t)0x080A0000) /* Base @ of Sector 9, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_10    ((uint32_t)0x080C0000) /* Base @ of Sector 10, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_11    ((uint32_t)0x080E0000) /* Base @ of Sector 11, 128 Kbytes */
+/**
+ * @brief Gets the sector of a given address
+ * @param None
+ * @retval The sector of a given address
+ */
+uint32_t GetSector(uint32_t Address)
+{
+	uint32_t sector = 0;
+
+	if((Address < ADDR_FLASH_SECTOR_1) && (Address >= ADDR_FLASH_SECTOR_0))
+	{
+	sector = FLASH_SECTOR_0;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_2) && (Address >= ADDR_FLASH_SECTOR_1))
+	{
+	sector = FLASH_SECTOR_1;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_3) && (Address >= ADDR_FLASH_SECTOR_2))
+	{
+	sector = FLASH_SECTOR_2;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_4) && (Address >= ADDR_FLASH_SECTOR_3))
+	{
+	sector = FLASH_SECTOR_3;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_5) && (Address >= ADDR_FLASH_SECTOR_4))
+	{
+	sector = FLASH_SECTOR_4;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_6) && (Address >= ADDR_FLASH_SECTOR_5))
+	{
+	sector = FLASH_SECTOR_5;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_7) && (Address >= ADDR_FLASH_SECTOR_6))
+	{
+	sector = FLASH_SECTOR_6;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_8) && (Address >= ADDR_FLASH_SECTOR_7))
+	{
+	sector = FLASH_SECTOR_7;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_9) && (Address >= ADDR_FLASH_SECTOR_8))
+	{
+	sector = FLASH_SECTOR_7;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_10) && (Address >= ADDR_FLASH_SECTOR_9))
+	{
+	sector = FLASH_SECTOR_7;
+	}
+	else if((Address < ADDR_FLASH_SECTOR_11) && (Address >= ADDR_FLASH_SECTOR_10))
+	{
+	sector = FLASH_SECTOR_7;
+	}
+	else if((Address < 0x080FFFFF) && (Address >= ADDR_FLASH_SECTOR_11))
+	{
+	sector = FLASH_SECTOR_7;
+	}
+	return sector;
+}
+
+
+//______________________________________________________________________________________________________
+
+
+uint32_t Int_to_Uint_Convert(int32_t _DATA_)
+{
+	return  (uint32_t)_DATA_;
+}
+
+void Int_to_Uint_16bit_Array_Convert(int16_t* _DATA_S, uint16_t* _DATA_D_, uint16_t _LENGTH_)
+{
+	for(uint16_t i = 0; i <_LENGTH_;i++)
+	{
+		_DATA_D_[i]  = (uint16_t)_DATA_S[i];
+	}
+}
+
+void Int_to_Uint_32bit_Array_Convert(int32_t* _DATA_S, uint32_t* _DATA_D_, uint16_t _LENGTH_)
+{
+	for(uint16_t i = 0; i <_LENGTH_;i++)
+	{
+		_DATA_D_[i]  = (uint32_t)_DATA_S[i];
+	}
+}
+
+int32_t Uint_to_Int_Convert(int32_t _DATA_)
+{
+	return  (int32_t)_DATA_;
+}
+
+void Uint_to_Int_16bit_Array_Convert(uint16_t* _DATA_S, int16_t* _DATA_D_, uint16_t _LENGTH_)
+{
+	for(uint16_t i = 0; i <_LENGTH_;i++)
+	{
+		_DATA_D_[i]  = (int16_t)_DATA_S[i];
+	}
+}
+
+void Uint_to_Int_32bit_Array_Convert(uint32_t* _DATA_S, int32_t* _DATA_D_, uint16_t _LENGTH_)
+{
+	for(uint16_t i = 0; i <_LENGTH_;i++)
+	{
+		_DATA_D_[i]  = (int32_t)_DATA_S[i];
+	}
+}
+
+
+
+
+/*
+    //_________STATIC FUNTION__________NO USE____
+
+*/
+
+//static void deleteBuffer8(uint8_t* data, uint16_t _LENGTH_)
+//{
+//	for(uint16_t i = 0; i < _LENGTH_; i++)
+//	{
+//		data[i] = 0;
+//	}
+//}
+//
+//static void deleteBuffer16(uint16_t* data, uint16_t _LENGTH_)
+//{
+//	for(uint16_t i = 0; i < _LENGTH_; i++)
+//	{
+//		data[i] = 0;
+//	}
+//}
+static void deleteBuffer32(uint32_t* data, uint16_t _LENGTH_)
+{
+	for(uint16_t i = 0; i < _LENGTH_; i++)
+	{
+		data[i] = 0;
+	}
+}
+// erase sector from addr
+void Flash_Erase(uint32_t addr, uint32_t numberSectorToErase)
+{ 
+	HAL_FLASH_Unlock();
+	uint32_t SectorError;
+	FLASH_EraseInitTypeDef EraseInitStruct;
+	EraseInitStruct.Banks = 1;
+	EraseInitStruct.Sector = GetSector(addr);
+	EraseInitStruct.NbSectors = numberSectorToErase;
+	EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
+	EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+	if(HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError) != HAL_OK)
+	{
+	/* Xảy ra lỗi tròn khi xóa Sector sẽ cần thêm một số code để xử lý lỗi này SectorError sẽ chứa sector bị lỗi,và sau đó để biết mã lỗi trên sector này bạn cần gọi hàm 'HAL_FLASH_GetError()'*/
+	/* FLASH_ErrorTypeDef errorcode = HAL_FLASH_GetError(); */
+		Error_Handler();
+	}
+	HAL_FLASH_Lock();
+}
+
+//______________________________________________________________________________________
+// READ FLASH FUNCTION
+/*
+ * - Des: this function read a 32 bit value from flash
+ * - para: @_ADDRESS_DATA_ : address we will get value
+ * - return the value read
+ * */
+uint32_t Flash_Read_Uint(uint32_t _ADDRESS_DATA_)
+{
+	return (*(__IO uint32_t*) _ADDRESS_DATA_);
+}
+/*
+ * - Des: this function read a string and return string to an array
+ * -para: @_Array_DATA_ : Array save read data
+ * 		  @_ADDRESS_DATA_: address start to read
+ * 		  @_LENGTH_ : byte to read
+ * */
+void Flash_Read_Array_32bit(uint32_t* _Array_DATA_, uint32_t _ADDRESS_DATA_, uint32_t _LENGTH_)
+{
+	deleteBuffer32(_Array_DATA_,_LENGTH_);
+	for(uint16_t i = 0; i < _LENGTH_; i++)
+	{
+		_Array_DATA_[i] = Flash_Read_Uint (_ADDRESS_DATA_ + i);
+	}
+}
+
+uint8_t Flash_Write_Uin32t(uint32_t _DATA_, uint32_t _ADDRESS_DATA_)
+{
+
+	Flash_Erase(_ADDRESS_DATA_, 1);
+	HAL_FLASH_Unlock();
+	HAL_FLASH_Program (FLASH_TYPEPROGRAM_WORD, _ADDRESS_DATA_, _DATA_);
+//	SET_BIT(FLASH->CR, FLASH_CR_PG);
+//	while((FLASH->SR&FLASH_SR_BSY)){};
+//	*(__IO uint16_t*)(_ADDRESS_DATA_) = (uint16_t)(_DATA_ & 0xFFFF) ;
+//				while((FLASH->SR&FLASH_SR_BSY)){};
+//	*(__IO uint16_t*)(_ADDRESS_DATA_ + 2U) = (uint16_t)((_DATA_ >> 16U) & 0xFFFF);
+//
+//	while((FLASH->SR&FLASH_SR_BSY)){};
+//	 CLEAR_BIT(FLASH->CR , FLASH_CR_PG);
+	HAL_FLASH_Lock();
+	uint32_t read_back = Flash_Read_Uint (_ADDRESS_DATA_);
+	if (read_back == _DATA_)
+	{
+		return 1; // send data ok
+	}
+	else
+	{
+		return 0; //send wrong data
+	}
+}
+uint8_t Flash_Write_Array_32bit(uint32_t* _Array_DATA_, uint32_t _ADDRESS_DATA_, uint32_t _LENGTH_)
+{
+	for(uint32_t i = 0; i < _LENGTH_; i++)
+	{
+		HAL_FLASH_Program (FLASH_TYPEPROGRAM_WORD, _ADDRESS_DATA_, _Array_DATA_[i]);
+	}
+	uint32_t Array_To_Check [_LENGTH_];// Creat a array to Check Back
+	Flash_Read_Array_32bit (Array_To_Check, _ADDRESS_DATA_, _LENGTH_);
+	if (memcmp (Array_To_Check, _Array_DATA_, _LENGTH_) == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
